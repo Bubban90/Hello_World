1dff47caf6f607358e75e682ab7672d011143dfb
Bubban90 <Bubban90@hotmail.com>
2017-02-07 13:50:00 +0100

    Add files via upload

---
+++ BinaryModell_v4/BinaryModell/BinaryGameModel.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryModell
+{
+    class BinaryGameModel
+    {
+        static public string deciToBinary(int deci)
+        {
+            string binary = Convert.ToString(deci, 2);
+            return binary;
+        }
+
+        static public string binaryToDeci(string binary)
+        {
+            string convertedBin = Convert.ToInt32(binary, 2).ToString();
+            return convertedBin;
+        }
+
+        static public string generateRandomBinary(int dif)
+        {
+            var seed = (int)DateTime.Now.Ticks;
+            Random rand = new Random(seed);
+
+            int num = 0;
+            string bin = null;
+            
+            if (dif == 1)
+            {
+                rand = new Random();
+                num = rand.Next(0, 16);
+                bin = deciToBinary(num);
+            }
+            else if (dif == 2)
+            {
+                rand = new Random();
+                num = rand.Next(16, 255);
+                bin = deciToBinary(num);
+            }
+            else if (dif == 3)
+            {
+                rand = new Random();
+                num = rand.Next(256, 4096);
+                bin = deciToBinary(num);
+            }
+            return bin;
+        }
+
+        static public string generateRandomDeci()
+        {
+            var seed = (int)DateTime.Now.Ticks;
+            Random rand = new Random(seed);
+            string randDeci = null;
+            randDeci = Convert.ToString(rand.Next(0, 16));
+            return randDeci;
+        }
+
+        static public string generateRandomHex(int dif)
+        {
+            int num = 0;
+            string hex = null;
+            Random rand;
+            if(dif == 1)
+            {
+                rand = new Random();
+                num = rand.Next(0, 16);
+                hex = deciToHex(num);
+            }
+            else if (dif == 2)
+            {
+                rand = new Random();
+                num = rand.Next(16, 255);
+                hex = deciToHex(num);
+            }
+            else if(dif == 3)
+            {
+                rand = new Random();
+                num = rand.Next(256, 4096);
+                hex = deciToHex(num);
+            }
+            return hex;
+        }
+
+        static public bool checkIfBinary(string testBin)
+        {
+            foreach (char c in testBin)
+            {
+                if (c != '0' && c!= '1')
+                {
+                    return false;
+                }
+                
+            }
+            return true;
+        }
+
+        static public bool checkIfDecimal(string testDeci)
+        {
+            foreach(char c in testDeci)
+            {
+                if(c < '1' || c > '9')
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        static public string deciToHex(int deci)
+        {
+            string hex = deci.ToString("X");
+            return hex;
+        }
+
+        static public string hexToDeci(string hex)
+        {
+            string convertedHex = Convert.ToInt32(hex, 16).ToString();
+            return convertedHex;
+        }
+
+        static private bool checkIfHex(string testHex)
+        {
+            bool isHex;
+            foreach (char c in testHex)
+            {
+                isHex = ((c >='0' && c <= '9')|| 
+                        (c>='a' && c<= 'f') ||
+                        (c>='A' && c<= 'F'));
+                if(!isHex)
+                    return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ BinaryModell_v4/BinaryModell/MainWindow.xaml.cs
@@ -0,0 +1,536 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Threading;
+
+namespace BinaryModell
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int _index = 1;
+        private int _correctAnswers = 0;
+        private string[] _questions = new String[20];
+        private string[] _answers = new String[20];
+        private bool _hexOrBin = true;
+        private int _difficulty = 0;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            generateExamQuestions();
+            this.examQuestion.Text = _questions[_index-1];
+        }
+
+        #region // Technical stuff
+
+        private void changeBin()
+        {
+            if(_difficulty == 1)
+            {
+                this.binaryQuestions.Text = BinaryGameModel.generateRandomBinary(_difficulty);
+
+            }
+            else if(_difficulty == 2)
+            {
+                this.binaryQuestions.Text = BinaryGameModel.generateRandomBinary(_difficulty);
+            }
+            else if (_difficulty == 3)
+            {
+                this.binaryQuestions.Text = BinaryGameModel.generateRandomBinary(_difficulty);
+            }
+        }
+
+        private void changeHex()
+        {
+            
+            if (_difficulty == 1)
+            {
+                this.binaryQuestions.Text = BinaryGameModel.generateRandomHex(_difficulty);
+            }
+            else if (_difficulty == 2)
+            {
+                this.binaryQuestions.Text = BinaryGameModel.generateRandomHex(_difficulty);
+            }
+            else if (_difficulty == 3)
+            {
+                this.binaryQuestions.Text = BinaryGameModel.generateRandomHex(_difficulty);
+            }
+        }
+            
+        private void generateExamQuestions()
+        {
+            _difficulty = 1;
+            for (int i = 0; i < 10; i++)
+            {
+                if(i == 4)
+                {
+                    _difficulty = 2;
+                }
+                else if(i > 7)
+                {
+                    _difficulty = 3;
+                }
+                _questions[i] = BinaryGameModel.generateRandomBinary(_difficulty);
+                _answers[i] = "";
+                Console.WriteLine(_questions[i]);
+                Thread.Sleep(10);
+            }
+            _difficulty = 2;
+            for (int j = 10; j <20; j++)
+            {
+                if(j > 17)
+                {
+                    _difficulty = 3;
+                }
+                _questions[j] = BinaryGameModel.generateRandomHex(_difficulty);
+                _answers[j] = "";
+                Console.WriteLine(_questions[j]);
+                Thread.Sleep(15);
+            }
+            _difficulty = 1;
+        }
+
+        private bool validateInput(string tempInput)
+        {
+            foreach (char c in tempInput)
+            {
+                if (c < '0' || c > '9')
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        private bool compareBinDeci()
+        {
+            string tempDeci = null;
+            string tempBin = null;
+            tempBin = this.binaryQuestions.Text;
+            tempDeci = BinaryGameModel.binaryToDeci(tempBin);
+
+            if (this.answer.Text == tempDeci)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private bool compareHexDeci()
+        {
+            string tempDeci = null;
+            string tempHex = null;
+            tempHex = this.binaryQuestions.Text;
+            tempDeci = BinaryGameModel.hexToDeci(tempHex);
+
+            if (this.answer.Text == tempDeci)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        #endregion
+
+        #region // All the buttons
+
+        private void menyBT_Click(object sender, RoutedEventArgs e)
+        {
+            testGrid.Visibility = Visibility.Collapsed;
+            menyGrid.Visibility = Visibility.Visible;
+        }
+
+        private void testButt_Click(object sender, RoutedEventArgs e)
+        {
+            menyGrid.Visibility = Visibility.Collapsed;
+            testGrid.Visibility = Visibility.Visible;
+
+            _difficulty = 1;
+            _hexOrBin = true;
+
+            if (_hexOrBin)
+            {
+                changeBin();
+                this.answer.Text = "";
+                this.textRuta.Content = "";
+                answer.Focus();
+            }
+            else
+            {
+                changeHex();
+                this.answer.Text = "";
+                this.textRuta.Content = "";
+                answer.Focus();
+            }
+        }
+
+        private void examButt_Click(object sender, RoutedEventArgs e)
+        {
+            menyGrid.Visibility = Visibility.Collapsed;
+            examGrid.Visibility = Visibility.Visible;
+        }
+
+        private void menyBExam_Click(object sender, RoutedEventArgs e)
+        {
+            examGrid.Visibility = Visibility.Collapsed;
+            menyGrid.Visibility = Visibility.Visible;
+            _index = 1;
+            generateExamQuestions();
+            this.examQuestion.Text = _questions[_index - 1];
+            this.TurnIn.IsEnabled = true;
+            this.previousButt.IsEnabled = false;
+            this.nextButt.IsEnabled = true;
+            this.answerBox.Text = "";
+            this.questionNumber.Content = _index + "/20";
+        }
+
+        private void enter_Button(object sender, KeyEventArgs e)
+        {
+            if(_hexOrBin)
+            {
+                bool ok = compareBinDeci();
+                if (e.Key == Key.Enter)
+                {
+                    if (ok)
+                    {
+                        this.textRuta.Content = "correct!";
+                    }
+                    else
+                    {
+                        this.textRuta.Content = "Wrong, try again!";
+                    }
+                }
+            }
+            else
+            {
+                bool ok = compareHexDeci();
+                if (e.Key == Key.Enter)
+                {
+                    if (ok)
+                    {
+                        this.textRuta.Content = "correct!";
+                    }
+                    else
+                    {
+                        this.textRuta.Content = "Wrong, try again!";
+                    }
+                }
+            }
+        }
+
+        private void TurnIn_Click_1(object sender, RoutedEventArgs e)
+        {
+
+            if (_index == 19)
+                _answers[19] = answerBox.Text.ToString();
+
+            //if (_answers[9] == BinaryGameModel.binaryToDeci(_questions[9]))
+            //    _correctAnswers++;
+            if (_answers[19] == BinaryGameModel.hexToDeci(_questions[19]))
+                _correctAnswers++;
+
+            if (TurnIn.Content.ToString() == "Play again?")
+            {
+                TurnIn.Content = "Turn In";
+                _correctAnswers = 0;
+                _index = 1;
+                generateExamQuestions();
+                this.examQuestion.Text = _questions[_index - 1];
+                this.questionNumber.Content = _index + "/20";
+                this.previousButt.IsEnabled = false;
+                this.nextButt.IsEnabled = true;
+            }
+            else
+            {
+                for (int i = 0; i < 9; i++)
+                {
+                    if (_answers[i] == "")
+                    {
+
+                    }
+                    else if (_answers[i] == BinaryGameModel.binaryToDeci(_questions[i]))
+                    {
+                        _correctAnswers++;
+                    }
+                }
+                for (int j = 10; j < 20; j++)
+                {
+                    if (_answers[j] == "")
+                    {
+
+                    }
+                    else if (_answers[j] == BinaryGameModel.hexToDeci(_questions[j]))
+                    {
+                        _correctAnswers++;
+                    }
+                }
+
+                questionNumber.Content = "You had " + _correctAnswers + " correct answers";
+                _correctAnswers = 0;
+                this.TurnIn.Content = "Play again?";
+            }
+
+
+            //this.TurnIn.IsEnabled = false;
+        }
+
+        private void previousButt_Click(object sender, RoutedEventArgs e)
+        {
+            answerBox.Focus();
+            _answers[_index - 1] = answerBox.Text;
+            --_index;
+            answerBox.Text = _answers[_index - 1];
+            this.questionNumber.Content = _index + "/20";
+            this.examQuestion.Text = _questions[_index - 1];
+            
+            if(_index == 1)
+            {
+                this.previousButt.IsEnabled = false;
+                this.nextButt.IsEnabled = true;
+            }
+            else if(_index > 1 && _index < 20)
+            {
+                this.nextButt.IsEnabled = true;
+            }
+        }
+
+        private void nextButt_Click(object sender, RoutedEventArgs e)
+        {
+            answerBox.Focus();
+            _answers[_index - 1] = answerBox.Text;
+            ++_index;
+            answerBox.Text = _answers[_index - 1];
+            this.questionNumber.Content = _index + "/20";
+            this.examQuestion.Text = _questions[_index - 1];
+
+            if(_index > 1 && _index < 20)
+            {
+                this.previousButt.IsEnabled = true;
+            }
+            else if (_index == 20)
+            {
+                this.nextButt.IsEnabled = false;
+            }
+        }
+
+        private void nextQue_Click(object sender, RoutedEventArgs e)
+        {
+            if (_hexOrBin)
+            {
+                changeBin();
+                this.answer.Text = "";
+                this.textRuta.Content = "";
+                answer.Focus();
+            }
+            else
+            {
+                changeHex();
+                this.answer.Text = "";
+                this.textRuta.Content = "";
+                answer.Focus();
+            }
+        }
+
+        #endregion
+
+        private void answerBox_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            
+            if (e.Key == Key.Enter)
+            {
+
+                if (_index > 19)
+                {
+                    _answers[_index - 1] = answerBox.Text;
+                    answerBox.Text = _answers[_index -1];
+                }
+                else
+                {
+                    _answers[_index - 1] = answerBox.Text;
+                    ++_index;
+                    answerBox.Text = _answers[_index - 1];
+                    this.questionNumber.Content = _index + "/20";
+                    this.examQuestion.Text = _questions[_index - 1];
+
+                    if (_index > 1 && _index < 20)
+                    {
+                        this.previousButt.IsEnabled = true;
+                    }
+                    else if (_index == 20)
+                    {
+                        this.nextButt.IsEnabled = false;
+                    }
+                }   
+            }
+
+        }
+
+        private void answer_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string tempInput = null;
+            tempInput = this.answer.Text;
+            if (validateInput(tempInput))
+            {
+                nextQue.IsEnabled = true;
+            }
+            else
+            {
+                nextQue.IsEnabled = false;
+            }
+        }
+
+        #region //Changes the difficulty for binary
+        /// <summary>
+        /// Makes diffrent difficulty for binary
+        /// </summary>
+        private void binEasy_Click(object sender, RoutedEventArgs e)
+        {
+            _difficulty = 1;
+            _hexOrBin = true;
+            if (_hexOrBin)
+            {
+                changeBin();
+                this.answer.Text = "";
+                this.textRuta.Content = "";
+                answer.Focus();
+            }
+            else
+            {
+                changeHex();
+                this.answer.Text = "";
+                this.textRuta.Content = "";
+                answer.Focus();
+            }
+        }
+
+        private void binMedium_Click(object sender, RoutedEventArgs e)
+        {
+            _difficulty = 2;
+            _hexOrBin = true;
+            if (_hexOrBin)
+            {
+                changeBin();
+                this.answer.Text = "";
+                this.textRuta.Content = "";
+                answer.Focus();
+            }
+            else
+            {
+                changeHex();
+                this.answer.Text = "";
+                this.textRuta.Content = "";
+                answer.Focus();
+            }
+        }
+
+        private void binHard_Click(object sender, RoutedEventArgs e)
+        {
+            _difficulty = 3;
+            _hexOrBin = true;
+            if (_hexOrBin)
+            {
+                changeBin();
+                this.answer.Text = "";
+                this.textRuta.Content = "";
+                answer.Focus();
+            }
+            else
+            {
+                changeHex();
+                this.answer.Text = "";
+                this.textRuta.Content = "";
+                answer.Focus();
+            }
+        }
+        #endregion
+
+        #region //Changes the difficulty for hex
+        /// <summary>
+        /// Changes the difficulty for hex
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void hexEasy_Click(object sender, RoutedEventArgs e)
+        {
+            _hexOrBin = false;
+            _difficulty = 1;
+            if (_hexOrBin)
+            {
+                changeBin();
+                this.answer.Text = "";
+                this.textRuta.Content = "";
+                answer.Focus();
+            }
+            else
+            {
+                changeHex();
+                this.answer.Text = "";
+                this.textRuta.Content = "";
+                answer.Focus();
+            }
+        }
+
+        private void hexMedium_Click(object sender, RoutedEventArgs e)
+        {
+            _difficulty = 2;
+            _hexOrBin = false;
+            if (_hexOrBin)
+            {
+                changeBin();
+                this.answer.Text = "";
+                this.textRuta.Content = "";
+                answer.Focus();
+            }
+            else
+            {
+                changeHex();
+                this.answer.Text = "";
+                this.textRuta.Content = "";
+                answer.Focus();
+            }
+        }
+
+        private void hexHard_Click(object sender, RoutedEventArgs e)
+        {
+            _difficulty = 3;
+            _hexOrBin = false;
+            if (_hexOrBin)
+            {
+                changeBin();
+                this.answer.Text = "";
+                this.textRuta.Content = "";
+                answer.Focus();
+            }
+            else
+            {
+                changeHex();
+                this.answer.Text = "";
+                this.textRuta.Content = "";
+                answer.Focus();
+            }
+        }
+        #endregion
+    }
+}
